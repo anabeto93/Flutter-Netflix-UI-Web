1c8e63d6b46990eb34872cd182140cb64e7a7d9a
Richard Anabeto Opoku <anabeto93@gmail.com>
2020-10-02 12:28:01 +0100

    started work on custom app bar.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/screens/screens.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,8 +15,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.black,
       ),
-      home: Scaffold(),
+      home: HomeScreen(),
     );
   }
 }
