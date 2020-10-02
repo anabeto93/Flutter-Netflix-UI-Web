e7fe138769fcc7d64ff63febb10db482f4333303
Richard Anabeto Opoku <anabeto93@gmail.com>
2020-10-02 18:40:48 +0100

    initial preview of contents show up.

--- lib/widgets/previews.dart
+++ lib/widgets/previews.dart
@@ -10,9 +10,50 @@ class Previews extends StatelessWidget {
     @required this.title,
     @required this.contentList,
   }) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
+          height: 165.0,
+          child: ListView.builder(
+              padding: EdgeInsets.symmetric(
+                vertical: 12.0,
+                horizontal: 8.0,
+              ),
+              scrollDirection: Axis.horizontal,
+              itemCount: contentList.length,
+              itemBuilder: (BuildContext context, int index) {
+                final Content content = contentList[index];
+                return GestureDetector(
+                  onTap: () => print(content.name),
+                  child: Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                        height: 130.0,
+                        width: 130.0,
+                        color: Colors.purple,
+                      ),
+                    ],
+                  ),
+                );
+              }),
+        ),
+      ],
+    );
   }
 }
